d0ba528540e37d732279933cb61f192a89a72003
chengjinghui123 <73371676+chengjinghui123@users.noreply.github.com>
2022-06-01 19:24:29 +0800

    pulltest

--- src/main/java/com/cjh/git/GitTest.java
+++ src/main/java/com/cjh/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hot-fix1");
+        System.out.println("pull test!");
     }
 }
